aa8b8925670837a18e0f89c6152695b9ae310066
Eric Holl <regentking@users.noreply.github.com>
2013-11-15 03:36:07 -0500

    Symfony tutorial

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
-namespace SourcedIn\BreadCrumbs;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-// todo
+$request = Request::createFromGlobals();
+$path = $request->getPathInfo();
+
+if (in_array($path, array('', '/')) {
+    $response = new Response('Welcome to the homepage for yolo octo robot.');
+elseif ($path == '/contact') {
+    $repsonse = new Response('Contact us.');
+}
+else {
+    $response = new Response("Page not found", 404);
+}
+$response->send();
