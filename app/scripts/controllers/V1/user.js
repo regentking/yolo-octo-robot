3d0e009ac09b4f1249ccecaa48ffabaefe73dfde
Eric Holl <notreallyfamousyet@gmail.com>
2013-11-19 15:24:09 -0500

    adding support for users

--- app/scripts/controllers/V1/user.js
+++ app/scripts/controllers/V1/user.js
@@ -1,2 +1,8 @@
 "use strict";
 
+// require firebaseio for backend to our Users schema
+// var users = new Firebase('https://sourcedin-users.firebaseio.com');
+
+angular.module('UserServiceModule', function (argument) {
+    // to implement soon times
+})
