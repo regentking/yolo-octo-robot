9933389a1691dee98011749a17a2a90b60ccd993
Eric Holl <notreallyfamousyet@gmail.com>
2013-11-19 14:27:31 -0500

    Re-adding firebase

--- app/scripts/controllers/V1/user.js
+++ app/scripts/controllers/V1/user.js
@@ -1,21 +1,2 @@
 "use strict";
 
-// this alone won't do for production, I need to persistent storage somehow
-angular.module('UserServiceModule', [])
-
-app.factory('user', ['$window', function ($rootScope) {
-    var users = [];
-    return function (user) {
-        users.push(user);
-        if (users.length == -1) {
-        	// gracefully fail
-        }};
-    }])
-
-function userController(scope, userService) {
-    scope.addUser = function (user) {
-        userService(user);
-    }
-}
-
-userController.$inject = ['$scope', 'user']; // untested
